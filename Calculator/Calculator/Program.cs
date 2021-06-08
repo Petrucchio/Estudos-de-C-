c5cd4f8d23af55633bfa0dbcea2ec41d3d7e4df6
Victor José Costa Farias <47372055+Petrucchio@users.noreply.github.com>
2021-06-08 14:39:38 -0300

    calculadora em C

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using CalculatorLibrary;
+
+namespace CalculatorProgram
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            Calculator calculator = new Calculator();
+            while (!endApp)
+            {
+                String numInput1 = "";
+                String numInput2 = "";
+                double result = 0;
+                string op;
+                Console.WriteLine("Console calculator in c# i guess");
+                Console.WriteLine("now just type a number, and press enter, ok?");
+                numInput1 = Console.ReadLine();
+                double number1 = 0;
+                while (!double.TryParse(numInput1, out number1))
+                {
+                    Console.WriteLine("Plaease enter with a valid number code");
+                    numInput1 = Console.ReadLine();
+                }
+                Console.WriteLine("now type another one");
+                numInput2 = Console.ReadLine();
+                double number2 = 0;
+                while (!double.TryParse(numInput2, out number2))
+                {
+                    Console.WriteLine("Plaease enter with a valid number code");
+                    numInput2 = Console.ReadLine();
+                }
+                Console.WriteLine("Choose an option from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.Write("Your option? ");
+                op = Console.ReadLine();
+
+                try
+                {
+                    result = calculator.DoOperation(number1, number2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("look here man, i actually just work oon numbers");
+                    }
+                    else
+                        Console.WriteLine($"yeaah it actually works dude , this is your result {result}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("yeah its not working");
+                }
+                Console.WriteLine("ok now press the key n to close this thing");
+                if (Console.ReadLine() == "n") endApp = true;
+                Console.WriteLine("\n"); // Friendly linespacing.
+            }
+            // And call to close the JSON writer before return
+            calculator.Finish();
+            return;
+        }
+    }
+}
---
+++ Calculator/CalculatorLibrary/CalulatorLibrary.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    
+    public class Calculator
+    {
+        JsonWriter writer;
+        public Calculator()
+        {
+            //create the json --------------------------------------------
+            StreamWriter logFile = File.CreateText("Calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+            //create the log --------------------------------------------
+            //StreamWriter logFile = File.CreateText("calculator.log");
+            //Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+            //Trace.AutoFlush = true;
+            //Trace.WriteLine("Starting Calculator Log");
+            //Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));
+        }
+        public double DoOperation(double num1, double num2, String op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" which we use if an operation, such as division, could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    //Trace.WriteLine(String.Format($"{num1} + {num2} = {result}"));//log
+                    writer.WriteValue("Add");//json
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    //Trace.WriteLine(String.Format($"{num1} - {num2} = {result}"));//log
+                    writer.WriteValue("Subtract");//json
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    //Trace.WriteLine(String.Format($"{num1} * {num2} = {result}"));//log
+                    writer.WriteValue("Multiply");//json
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        //Trace.WriteLine(String.Format($"{num1} / {num2} = {result}"));//log
+                        writer.WriteValue("Divide");//json
+                    }
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");//json
+            writer.WriteValue(result);//json
+            writer.WriteEndObject();//json
+            return result;
+        }
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+    }
+}
